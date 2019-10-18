f4e79c470eacaef948365aac8703fa21880a1497
Vlad <vlad169@yandex.ru>
2019-10-18 14:31:11 +0300

    explorer folder-file

--- les1/explorer.php
+++ les1/explorer.php
@@ -14,10 +14,30 @@ if ($_SERVER['QUERY_STRING']){
 }
 
 $dir = new DirectoryIterator($pathString);
+$str = '';
 
 foreach ($dir as $item) {
-    echo "<p><a href='?path={$item}'>{$item}</a></p>";
+    if($item == '.') continue;
+
+    //проверка для определения картинки
+    if($dir->getType() === 'dir')
+        $img = 'folder.jpg';
+    else
+        $img = 'file.png';
+
+    $str .= "<p><img src='img/{$img}' alt='' width='20px' height='20px'> ";
+
+    //проверка, если папка - сделать ссылкой
+    if($dir->getType() === 'dir')
+        $str .= "<a href='?path={$item}'>{$item}</a></p>";
+    else
+        $str .= "{$item}</p>";
+
+
+
+//    echo "<p><img src='img/{$img}' alt='' width='20px' height='20px'><a href='?path={$item}'>{$item}</a></p>";
 }
+echo $str;
 
 
 
